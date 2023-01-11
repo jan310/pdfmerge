4a14ddfe7817f1119de7a0302972121485a1b895
Jan Ondra <jan310.ondra@gmail.com>
2023-01-11 10:35:41 +0100

    outsourced logic to a service

---
+++ src/main/kotlin/com/ondra/pdfmerge/controller/MergePdfController.kt
@@ -0,0 +1,32 @@
+package com.ondra.pdfmerge.controller
+
+import com.ondra.pdfmerge.model.MergeSpecification
+import com.ondra.pdfmerge.service.MergePdfService
+import org.springframework.http.*
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("/api/pdf")
+class MergePdfController(private val mergePdfService: MergePdfService) {
+
+    @PostMapping("/merge-files", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_PDF_VALUE])
+    fun mergeFiles(@RequestPart files: Array<MultipartFile>): ResponseEntity<ByteArray> {
+        val headers = HttpHeaders()
+        headers.contentDisposition = ContentDisposition.builder("attachment").filename("merged.pdf").build()
+
+        return ResponseEntity(mergePdfService.mergeFiles(files), headers, HttpStatus.CREATED)
+    }
+
+    @PostMapping("/merge-pages", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE], produces = [MediaType.APPLICATION_PDF_VALUE])
+    fun mergePages(@RequestPart files: Array<MultipartFile>, @RequestPart mergeSpecification: MergeSpecification): ResponseEntity<ByteArray> {
+        val headers = HttpHeaders()
+        headers.contentDisposition = ContentDisposition.builder("attachment").filename("merged.pdf").build()
+
+        return ResponseEntity(mergePdfService.mergePages(files, mergeSpecification), headers, HttpStatus.CREATED)
+    }
+
+}
---
+++ src/main/kotlin/com/ondra/pdfmerge/service/MergePdfService.kt
@@ -0,0 +1,56 @@
+package com.ondra.pdfmerge.service
+
+import com.itextpdf.text.Document
+import com.itextpdf.text.pdf.PdfCopy
+import com.itextpdf.text.pdf.PdfReader
+import com.ondra.pdfmerge.model.MergeSpecification
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayOutputStream
+
+@Service
+class MergePdfService {
+
+    fun mergeFiles(files: Array<MultipartFile>): ByteArray {
+        val document = Document()
+        val outputStream = ByteArrayOutputStream()
+        val pdfCopy = PdfCopy(document, outputStream)
+
+        document.open()
+
+        files.forEach {
+            val pdfReader = PdfReader(it.inputStream)
+            val numberOfPages = pdfReader.numberOfPages
+            for (i in 1..numberOfPages) {
+                pdfCopy.addPage(pdfCopy.getImportedPage(pdfReader,i))
+            }
+            pdfReader.close()
+        }
+
+        document.close()
+
+        return outputStream.toByteArray()
+    }
+
+    fun mergePages(files: Array<MultipartFile>, mergeSpecification: MergeSpecification): ByteArray {
+        val document = Document()
+        val outputStream = ByteArrayOutputStream()
+        val pdfCopy = PdfCopy(document, outputStream)
+
+        val pdfReaderList = files.map { PdfReader(it.inputStream) }
+
+        document.open()
+
+        mergeSpecification.fileSpecifications.forEach { fileSpec ->
+            fileSpec.pageNumbers.forEach { pageNumber ->
+                pdfCopy.addPage(pdfCopy.getImportedPage(pdfReaderList[fileSpec.fileNumber], pageNumber))
+            }
+            pdfReaderList[fileSpec.fileNumber].close()
+        }
+
+        document.close()
+
+        return outputStream.toByteArray()
+    }
+
+}
