d9f2715d629904a576ebae4606b59a9bc79f6deb
Jan Ondra <jan310.ondra@gmail.com>
2023-01-12 08:30:45 +0100

    enabled CORS

--- src/main/kotlin/com/ondra/pdfmerge/controller/MergePdfController.kt
+++ src/main/kotlin/com/ondra/pdfmerge/controller/MergePdfController.kt
@@ -2,13 +2,19 @@ package com.ondra.pdfmerge.controller
 
 import com.ondra.pdfmerge.model.MergeSpecification
 import com.ondra.pdfmerge.service.MergePdfService
-import org.springframework.http.*
+import org.springframework.http.ContentDisposition
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/pdf")
 class MergePdfController(private val mergePdfService: MergePdfService) {
@@ -17,7 +23,6 @@ class MergePdfController(private val mergePdfService: MergePdfService) {
     fun mergeFiles(@RequestPart files: Array<MultipartFile>): ResponseEntity<ByteArray> {
         val headers = HttpHeaders()
         headers.contentDisposition = ContentDisposition.builder("attachment").filename("merged.pdf").build()
-
         return ResponseEntity(mergePdfService.mergeFiles(files), headers, HttpStatus.CREATED)
     }
 
@@ -25,7 +30,6 @@ class MergePdfController(private val mergePdfService: MergePdfService) {
     fun mergePages(@RequestPart files: Array<MultipartFile>, @RequestPart mergeSpecification: MergeSpecification): ResponseEntity<ByteArray> {
         val headers = HttpHeaders()
         headers.contentDisposition = ContentDisposition.builder("attachment").filename("merged.pdf").build()
-
         return ResponseEntity(mergePdfService.mergePages(files, mergeSpecification), headers, HttpStatus.CREATED)
     }
 
